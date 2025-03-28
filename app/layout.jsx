453851a2f25f85d9a42a9617fb4092066ecf3be2
Leticia Rosa <leticiarosadeoliveira@yahoo.com.br>
2025-03-27 23:24:17 -0300

    Using next 15

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      Root
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ app/not-found.jsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+  return (
+    <div>
+      Você está perdido?
+    </div>
+  )
+}
